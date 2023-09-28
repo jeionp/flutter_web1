db46087beee70621926dbacd043797d566146983
jeionp <wabisabisoba@Jermaines-Mac-mini.local>
2023-09-28 20:54:49 +0900

    getx implementation

---
+++ lib/screens/other.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_web1/controllers/counterController.dart';
+import 'package:get/get.dart';
+
+class OtherScreen extends StatelessWidget {
+  final CounterController _counterController = Get.find();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text("Screen was clicked ${_counterController.counter.value} times"),
+          SizedBox(height:10),
+          ElevatedButton(
+            onPressed: () {
+              Get.back();
+            }, 
+            child: Text("Open Other Screen"),
+          )
+        ]
+      ),
+    );
+  }
+}
